2c073834b2a116e8225297e3c4f7181fe6f1bafe
A_A <21040751+Otto-AA@users.noreply.github.com>
2019-03-02 17:11:30 +0100

    Add build folder

---
+++ build/precache-manifest.c25bbfc9ac4e8c4d55ddaedbe7daf3ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef6f45f6174f2c0ec922",
+    "url": "/solid-filemanager/static/js/runtime~main.ef6f45f6.js"
+  },
+  {
+    "revision": "c11f9755653e8c898a7d",
+    "url": "/solid-filemanager/static/js/main.c11f9755.chunk.js"
+  },
+  {
+    "revision": "a3e1100f8bf0b11c05d6",
+    "url": "/solid-filemanager/static/js/1.a3e1100f.chunk.js"
+  },
+  {
+    "revision": "c11f9755653e8c898a7d",
+    "url": "/solid-filemanager/static/css/main.62f02726.chunk.css"
+  },
+  {
+    "revision": "1c67590236072a79f2855e4c8b37b5be",
+    "url": "/solid-filemanager/index.html"
+  }
+];
