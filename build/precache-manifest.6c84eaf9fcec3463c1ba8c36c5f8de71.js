815fddcc2d10a2bd2b89a663af259f18d8604315
A_A <21040751+Otto-AA@users.noreply.github.com>
2019-03-05 19:23:22 +0100

    Make build

--- build/precache-manifest.6c84eaf9fcec3463c1ba8c36c5f8de71.js
+++ build/precache-manifest.6c84eaf9fcec3463c1ba8c36c5f8de71.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/solid-filemanager/build/static/js/runtime~main.858ab442.js"
   },
   {
-    "revision": "97d71dc889ac9f4a4cc4",
-    "url": "/solid-filemanager/build/static/js/main.97d71dc8.chunk.js"
+    "revision": "bb300a69be2087605606",
+    "url": "/solid-filemanager/build/static/js/main.bb300a69.chunk.js"
   },
   {
     "revision": "ed8780a29b2328e4d412",
     "url": "/solid-filemanager/build/static/js/2.ed8780a2.chunk.js"
   },
   {
-    "revision": "97d71dc889ac9f4a4cc4",
+    "revision": "bb300a69be2087605606",
     "url": "/solid-filemanager/build/static/css/main.5ad03d98.chunk.css"
   },
   {
-    "revision": "aa9d66b7b422797e3bc8835b6cd7821b",
+    "revision": "07920e84ab3d2278ab9dbfb062fb4214",
     "url": "/solid-filemanager/build/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/solid-filemanager/build/precache-manifest.3e373b906d332ceb59edd57b91d2fbc2.js"
+  "/solid-filemanager/build/precache-manifest.6c84eaf9fcec3463c1ba8c36c5f8de71.js"
 );
 
 workbox.clientsClaim();
