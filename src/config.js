7affba5fe0effc328b6ab6c79ef5ab321ac0af33
A_A <21040751+Otto-AA@users.noreply.github.com>
2019-03-05 19:47:49 +0100

    Remove slash at the end of location storage

--- src/config.js
+++ src/config.js
@@ -6,6 +6,8 @@ export default {
     },
     setHost(newHost) {
         host = newHost;
+        while (host.slice(-1) === '/')
+            host = host.slice(0, -1);
     },
 
     isEditableFilePattern: /\.(txt|diff?|patch|svg|asc|cnf|cfg|conf|html?|cfm|cgi|aspx?|ini|pl|py|md|css|cs|jsx?|jsp|log|htaccess|htpasswd|gitignore|gitattributes|env|json|atom|eml|rss|markdown|sql|xml|xslt?|sh|rb|as|bat|cmd|cob|for|ftn|frm|frx|inc|lisp|scm|coffee|php[3-6]?|java|c|cbl|go|h|scala|vb|tmpl|lock|go|yml|yaml|tsv|lst|ttl)$/i,
