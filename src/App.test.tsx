20280a4d7d446395185ee3a78099531b59dbf971
A_A <21040751+Otto-AA@users.noreply.github.com>
2020-04-28 07:55:08 +0200

    Update sanity test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+import reducer from './Reducers/reducer'
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider >
+  , div);
   ReactDOM.unmountComponentAtNode(div);
 });
