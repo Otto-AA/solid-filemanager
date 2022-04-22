4640c8e392d2a5c5adf017fd03587dd2babc8aaf
A_A <21040751+Otto-AA@users.noreply.github.com>
2022-04-22 14:05:51 +0200

    disable service worker. Ref #26

--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+// see issue https://github.com/Otto-AA/solid-filemanager/issues/26
+serviceWorker.unregister();
+/*
 serviceWorker.register({
     onUpdate: (config) => {
         console.group('serviceWorker.onUpdate');
@@ -32,3 +35,4 @@ serviceWorker.register({
         console.groupEnd();
     }
 });
+*/
